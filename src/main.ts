a901c74ca769918b85e402c2cbff3fe191d1ef5f
Bayma Bruno <baymabruno@gmail.com>
2021-05-08 16:51:41 -0300

    adicionado módulo para views

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const logger = new Logger();
+  const port = process.env.PORT || 3000;
+
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Nest Start Project')
+    .setDescription('Projeto start Node.Js - NestJs')
+    .setVersion('2.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    allowedHeaders: 'Content-Type, Authorization, X-Requested-With',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    maxAge: 86400,
+    credentials: true,
+  });
+
+  await app.listen(port);
+
+  logger.log(`Server is listening on port: ${await app.getUrl()}`);
 }
 bootstrap();
